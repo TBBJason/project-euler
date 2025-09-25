e177d9f9782ee66cd2b04506cd3d838b6dd8d576
John Doe <john.doe@example.com>
2025-09-25 11:07:27 -0400

    largest prime start

---
+++ Largest-Prime.py
@@ -0,0 +1,8 @@
+# Find the largest prime factor of a number
+
+
+"""
+This was apart of cs135's problems, didn't know how to do it then, still don't remember the most optimal
+I remember goldbach's conjecture though, since lots of my tests were very slow, we didn't have to check all the way up until
+the number n, but we could guarantee sqrt(n) was sufficient.
+"""
